ebf297712fc045b99ea24976e3a2632351ce1036
Nikolay Todorov <nikolaytodorov3411@gmail.com>
2018-11-16 14:08:00 +0200

    adding starting project

---
+++ script.js
@@ -0,0 +1,40 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+function generateRandomColor() {
+    body.style.background = "linear-gradient(to right, " 
+    + getRandomColor()
+    + ", " 
+    + getRandomColor()
+    + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+function setGradient(){
+    body.style.background = "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value 
+    + ")";
+    
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", generateRandomColor);
